96c95bc1b51ea53e103e0b4707ff54bf4606f7f2
SaraVanchova <125595064+SaraVanchova@users.noreply.github.com>
2023-04-11 18:54:43 +0200

    docx and terraform code

--- Homework No.20 Terraform/Task 2 and 3/inputs.tfvars
+++ Homework No.20 Terraform/Task 2 and 3/inputs.tfvars
@@ -1,1 +1,1 @@
-my_name = "Sara"
+my_name = "sara"
--- Homework No.20 Terraform/Task 2 and 3/main.tf
+++ Homework No.20 Terraform/Task 2 and 3/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "example" {
      location = var.location 
 } 
 resource "azurerm_storage_account" "example" { 
-    name = "${random_string.random.result}sa" 
+    name = "${local.resource_prefix}sa" 
     resource_group_name = azurerm_resource_group.example.name 
     location = azurerm_resource_group.example.location 
     account_tier = "Standard" 
