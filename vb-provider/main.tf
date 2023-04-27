85bec79c3924c0e5cec79170f6b518b8f77ac96d
SaraVanchova <125595064+SaraVanchova@users.noreply.github.com>
2023-04-27 12:23:18 +0200

    test

--- vb-provider/main.tf
+++ vb-provider/main.tf
@@ -13,7 +13,7 @@ provider "virtualbox" {
 resource "virtualbox_vm" "node" {
   count     = 2
   name      = format("node-%02d", count.index + 1)
- # image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 2
   memory    = "512 mib"
   #user_data = file("${path.module}/user_data")
